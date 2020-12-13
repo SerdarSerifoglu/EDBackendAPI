6bd2102d133e7df5fd917a6f07ede25ed89de7a7
serdar serif <serdarserifoglu@gmail.com>
2020-12-13 11:19:27 +0300

    Business katmanının kodları yazıldı.

---
+++ EDBackendAPI.Business/Abstract/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using EDBackendAPI.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDBackendAPI.Business.Abstract
+{
+    public interface IProductService
+    {
+        Task<Product> GetById(int productId);
+        Task<List<Product>> GetList();
+        Task<List<Product>> GetListByCategory(int categoryId);
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+    }
+}
---
+++ EDBackendAPI.Business/Concrete/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using EDBackendAPI.Business.Abstract;
+using EDBackendAPI.DataAccess.Abstract;
+using EDBackendAPI.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDBackendAPI.Business.Concrete
+{
+    public class ProductService : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductService(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public Task<Product> GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
+        public Task<List<Product>> GetList()
+        {
+            return _productDal.GetList();
+        }
+
+        public Task<List<Product>> GetListByCategory(int categoryId)
+        {
+            return _productDal.GetList(p => p.CategoryId == categoryId);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
