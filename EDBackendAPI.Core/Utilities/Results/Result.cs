0dc67453f3d07b232ab8debc7ebae3f5595a0135
serdar serif <serdarserifoglu@gmail.com>
2020-12-13 11:33:20 +0300

    Core katmanında Result yapısı oluşturuldu.

---
+++ EDBackendAPI.Core/Utilities/Results/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDBackendAPI.Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //this(success) yazmayıp, alttaki constructor'a "Success = success;" yazabilirdik. kod tekrarı olmasın diye bu şekilde kullandık.
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
