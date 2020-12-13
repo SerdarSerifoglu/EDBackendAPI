6d4e0b821ee4f37d082f7cb788c680960a8b3205
serdar serif <serdarserifoglu@gmail.com>
2020-12-13 14:00:42 +0300

    Business katmanına Autofac kuruldu ve ilk module oluşturuldu.

---
+++ EDBackendAPI.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using EDBackendAPI.Business.Abstract;
+using EDBackendAPI.Business.Concrete;
+using EDBackendAPI.DataAccess.Abstract;
+using EDBackendAPI.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDBackendAPI.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductService>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+    }
+}
