8cedfae432c94c6ec340b3d2b9bc582ecd3159d5
serdar serif <serdarserifoglu@gmail.com>
2020-12-14 22:35:13 +0300

    User Rol yapısı için Entity'ler oluşturuldu.

---
+++ EDBackendAPI.Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,14 @@
+﻿using EDBackendAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDBackendAPI.Entities.Concrete
+{
+    public class UserOperationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
