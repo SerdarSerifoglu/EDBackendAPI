b2bed175b4d408419143ee432a55fb20be05a5c0
serdar serif <serdarserifoglu@gmail.com>
2020-12-19 16:20:42 +0300

    Business katmanına UserService eklendi.

---
+++ EDBackendAPI.Business/Concrete/UserService.cs
@@ -0,0 +1,33 @@
+﻿using EDBackendAPI.Business.Abstract;
+using EDBackendAPI.DataAccess.Abstract;
+using EDBackendAPI.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDBackendAPI.Business.Concrete
+{
+    public class UserService : IUserService
+    {
+        IUserDal _userDal;
+        public UserService(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public void Add(User user)
+        {
+            _userDal.Add(user);
+        }
+
+        public User GetByMail(string email)
+        {
+            return _userDal.Get(x => x.Email == email);
+        }
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+            return _userDal.GetClaims(user);
+        }
+    }
+}
