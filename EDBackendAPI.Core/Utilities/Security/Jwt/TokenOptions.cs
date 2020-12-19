b5f72ca132c91c284a163a90ff44cb737d4f99e1
serdar serif <serdarserifoglu@gmail.com>
2020-12-19 19:52:53 +0300

    Jwt Token için altyapı kurulumuna başlandı.

---
+++ EDBackendAPI.Core/Utilities/Security/Jwt/TokenOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDBackendAPI.Core.Utilities.Security.Jwt
+{
+    public class TokenOptions
+    {
+        public string Audience { get; set; } //TR Anlam, Audience: İzleyici
+        public string Issuer { get; set; }//TR Anlam, Issuer: Sağlayıcı
+        public int AccessTokenExpiration { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
