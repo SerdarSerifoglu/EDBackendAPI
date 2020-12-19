9d5e4ece46892d95c4afe63fe8ed3b70685689fb
serdar serif <serdarserifoglu@gmail.com>
2020-12-19 16:22:40 +0300

    UserDal ve UserService için Autofac eklemesi yapıldı.

--- EDBackendAPI.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ EDBackendAPI.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -15,6 +15,9 @@ namespace EDBackendAPI.Business.DependencyResolvers.Autofac
         {
             builder.RegisterType<ProductService>().As<IProductService>();
             builder.RegisterType<EfProductDal>().As<IProductDal>();
+
+            builder.RegisterType<UserService>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
         }
     }
 }
