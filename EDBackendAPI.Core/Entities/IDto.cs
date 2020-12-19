6eef044638ea0d0bbb293a2ffc1be14dfe98bde2
serdar serif <serdarserifoglu@gmail.com>
2020-12-19 16:29:27 +0300

    Login ve Register işlemlerinde kullanılacak Dto'lar projeye eklendi.

---
+++ EDBackendAPI.Core/Entities/IDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDBackendAPI.Core.Entities
+{
+    public interface IDto
+    {
+    }
+}
---
+++ EDBackendAPI.Entities/Dtos/UserForLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using EDBackendAPI.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDBackendAPI.Entities.Dtos
+{
+    public class UserForLoginDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
