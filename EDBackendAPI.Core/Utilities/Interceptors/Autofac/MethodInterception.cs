2f70247e15e89062791abe1ad6f53e3be44783e3
serdar serif <serdarserifoglu@gmail.com>
2020-12-27 20:31:43 +0300

    Method Interception altyapısı oluşturuldu

---
+++ EDBackendAPI.Core/Utilities/Interceptors/Autofac/MethodInterception.cs
@@ -0,0 +1,39 @@
+﻿using Castle.Core.Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDBackendAPI.Core.Utilities.Interceptors.Autofac
+{
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation ınvocation) { }
+        protected virtual void OnAfter(IInvocation ınvocation) { }
+        protected virtual void OnException(IInvocation ınvocation) { }
+        protected virtual void OnSuccess(IInvocation ınvocation) { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception)
+            {
+                isSuccess = false;
+                OnException(invocation);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
